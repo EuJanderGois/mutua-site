873b1e388bf3c828a711256c8abd6748c1271576
EuJanderGois <jender.man33@gmail.com>
2025-09-21 17:58:23 -0300

    Diversas melhorias na estrutura do projeto, otimizações nos estilos, melhorias relacionadas a responsividade

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
+import "./styles/globals.css";
+
+import "./styles/components/body.css";
+import "./styles/components/header.css";
+import "./styles/components/slider.css";
+
 
 window.onload = () => {
   console.log("[Mútua] Client is ready ...");
